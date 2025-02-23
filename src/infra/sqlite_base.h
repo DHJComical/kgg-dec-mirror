4a132bfa6d1fec67286f62d12db51cb701152d72
Comical <98379882+DHJComical@users.noreply.github.com>
2025-02-23 08:05:26 +0800

    Delete src directory

--- src/infra/sqlite_base.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// SQLite
-#define SQLITE_OPEN_READONLY 0x00000001  /* Ok for sqlite3_open_v2() */
-#define SQLITE_OPEN_READWRITE 0x00000002 /* Ok for sqlite3_open_v2() */
-#define SQLITE_OPEN_CREATE 0x00000004    /* Ok for sqlite3_open_v2() */
-
-typedef struct sqlite3 sqlite3;
-typedef struct sqlite3_stmt sqlite3_stmt;
